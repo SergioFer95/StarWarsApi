748f9807eacc37fd50f2f1effa4ff0addc4c93f8
Sergio Fernández <sergio.fernndez@bootcamp-upgrade.com>
2022-01-18 10:57:58 +0100

    Add API url and try to get info

--- SRC/style.js
+++ SRC/style.js
@@ -1,6 +1,12 @@
-'use strict';
 
-window.addEventListener("load", ()=>{
-
+window.onLoad = () =>{
     init();
-})
+}
+
+const init = async ()=>{
+    const getApiInfo = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+    const transformData = await getApiInfo.json();
+    console.log(transformData);
+
+    return transformData;
+}
