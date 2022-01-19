6c0a72e42942edac37a44238a5fdcfc41826c3cd
Sergio Fernández <sergio.fernndez@bootcamp-upgrade.com>
2022-01-19 11:31:47 +0100

    Add create new <li> in HTML´s file with JS

--- SRC/style.js
+++ SRC/style.js
@@ -1,12 +1,36 @@
 
-window.onLoad = () =>{
-    init();
+window.onload = () => {
+
+    var usuarios= [];
+
+    fetch('https://reqres.in/api/users')
+    .then(data => data.json())
+    .then(users =>{
+        usuarios = users.data;
+    })
+
+    console.log(usuarios);
+
+
+
+
+
+
+
+    // Apartado CREAR UN NUEVO <li>, HECHO:
+    let ourUl = document.querySelector('#pokeListOne');
+
+    const createNewLi = () => {
+        let createLi = document.createElement("li");
+        createLi.innerHTML = `
+        Hola, soy texto`
+
+        ourUl.append(createLi);
+    }
+
+    createNewLi();
+
 }
 
-const init = async ()=>{
-    const getApiInfo = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
-    const transformData = await getApiInfo.json();
-    console.log(transformData);
 
-    return transformData;
-}
+
